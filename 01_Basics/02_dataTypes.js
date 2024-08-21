0c2ddb4f3d4d36cb0b138bc6e31d19a961644a00
harshb445 <98596821+harshb445@users.noreply.github.com>
2024-08-21 10:27:30 +0000

    02_datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all the JS code as new version
+
+// alert(3+3)  we're using node js not browser
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// symbol => unique
+
+//object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
